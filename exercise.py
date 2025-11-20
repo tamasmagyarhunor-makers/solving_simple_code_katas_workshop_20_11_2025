1c7c4b8b31199bcd4700ed1606a7370d189045fd
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2025-11-20 10:27:13 +0000

    Solve exercise 1

--- exercise.py
+++ exercise.py
@@ -8,6 +8,20 @@
 # Side effects
 #    - None
 def make_initials(fullname):
-    pass
+    # return fullname.split(" ")[0][0] + "." + fullname.split(" ")[1][0]
+    names = fullname.split(" ") # task 1
+
+    first_name = names[0]
+    first_name_char = first_name[0]
+
+    last_name = names[1]
+    last_name_char = last_name[0] # task 2
+    
+    return first_name_char + "." + last_name_char # task 3
+
+# Tasks
+# - split the fullname with a space #=> ['name', 'name'] DONE
+# - we need to take out the first characters (looping through, accessing them or looking for capitals) DONE
+# - we need to return the first characters with a "." in between DONE
 
 print(make_initials('Will Smith'))
